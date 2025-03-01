b8a032910805d26b546c133fd08613da6168e2c8
Yolo <g20180630105@gmail.com>
2025-03-01 18:16:48 +0800

    feat: footer

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,18 @@
 import Nav from "@/components/nav";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <div>
       <Nav />
-      <div className="p-5 content-height">Home</div>
+      <div className="p-5 content-height">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <div key={i} className="mb-5 bg-slate-200">
+            {i}
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,40 @@
+import { Github, Twitter } from "lucide-react";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+
+import { Button } from "@/components/ui/button";
+
+export default function Footer() {
+  const t = useTranslations();
+
+  return (
+    <div className="w-full min-h-20 px-5 bg-background text-foreground flex items-start">
+      <div className="w-60">
+        <div className="text-2xl font-bold mb-5">NEXT APP</div>
+        <Link className="underline" href="g20180630105@gmail.com">
+          g20180630105@gmail.com
+        </Link>
+        <div className="font-semibold my-5">{t("layout.footer.follow")}</div>
+        <div className="flex gap-5">
+          <Link href="https://github.com/Yolo-00/next-app">
+            <Button
+              variant="ghost"
+              className="w-8 h-8 [&_svg]:size-5 bg-accent"
+            >
+              <Github strokeWidth={2} />
+            </Button>
+          </Link>
+
+          <Link href="https://x.com/Yolo__00">
+            <Button
+              variant="ghost"
+              className="w-8 h-8 [&_svg]:size-5 bg-accent"
+            >
+              <Twitter strokeWidth={2} />
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
